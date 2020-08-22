6f3f42e47c18cdc649786622bc68bce02ae09124
unknown <44569804+110yinon@users.noreply.github.com>
2020-08-23 00:54:29 +0300

    functions decalarations and expressions

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,13 @@
-let score = '100' // string
+// functions declaration
+function greet(){
+    console.log('hello there');
+}
 
-console.log(score + 1); // 1001 string
-console.log(typeof (score));
+// function expression
+const speak = function(){
+    console.log('good day!');
+}
 
-score = Number(score) // Number
-console.log(score + 1); // 101
-console.log(typeof (score));
-
-console.log(Number("hello")); // NaN = Not A Number
-
-let result = 200 // Number
-console.log(typeof result);
-
-result = String(result) // string
-console.log(typeof result);
-
-result = 'true'; // string
-console.log(typeof result);
-
-result = Boolean(result); // boolean
-console.log(typeof result);
+// functions invoke
+greet();
+speak();
