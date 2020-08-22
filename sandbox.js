2b348ae6c8565e01b10a69c3f2af10a27d45dd9e
unknown <44569804+110yinon@users.noreply.github.com>
2020-08-22 22:57:10 +0300

    type conversion

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,22 @@
-alert('bb the king')
+let score = '100' // string
+
+console.log(score + 1); // 1001 string
+console.log(typeof (score));
+
+score = Number(score) // Number
+console.log(score + 1); // 101
+console.log(typeof (score));
+
+console.log(Number("hello")); // NaN = Not A Number
+
+let result = 200 // Number
+console.log(typeof result);
+
+result = String(result) // string
+console.log(typeof result);
+
+result = 'true'; // string
+console.log(typeof result);
+
+result = Boolean(result); // boolean
+console.log(typeof result);
