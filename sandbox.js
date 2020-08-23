cecbb49e57c69587a31a680952ee5406cd7422e2
unknown <44569804+110yinon@users.noreply.github.com>
2020-08-23 20:14:27 +0300

    web dev simplified promise and callbacks

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,59 @@
-// get reference to the UI
-const ui = document.querySelector('.people');
+const userLeft = false;
+const userWatchingCatMeme = false;
 
-const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+function watchTutorialCallback(callback, errorCallback) {
+    if (userLeft) {
+        errorCallback({
+            name: 'User Left',
+            message: ':('
+        })
+    } else if (userWatchingCatMeme) {
+        errorCallback({
+            name: 'User Watching Cat Meme',
+            message: 'WebDevSimplifeid < Cat'
+        })
+    } else {
+        callback('Thumbs up and subscribe')
+    }
+}
 
-let html = ``;
 
-people.forEach(person => {
-    // create html template
-    html += `<li style="color: purple">${person}</li>`
-});
+watchTutorialCallback(msg => {
+    console.log('Success: ' + msg);
+}, err => {
+    console.log(err.name + ' ' + err.message);
+})
 
-console.log(html);
-ui.innerHTML = html;
+// *****************************************
+// *****************************************
+// *****************************************
+// *****************************************
+// *****************************************
+// *****************************************
+
+function watchTutorialCallbackPromise() {
+    return new Promise((callback, errorCallback) => {
+        if (userLeft) {
+            errorCallback({
+                name: 'User Left',
+                message: ':('
+            })
+        } else if (userWatchingCatMeme) {
+            errorCallback({
+                name: 'User Watching Cat Meme',
+                message: 'WebDevSimplifeid < Cat'
+            })
+        } else {
+            callback('Thumbs up and subscribe')
+        }
+    });
+}
+
+console.log('*****************************');
+
+
+watchTutorialCallbackPromise().then(msg => {
+    console.log('Success: ' + msg);
+}).catch(err => {
+    console.log(err.name + ' ' + err.message);
+})
