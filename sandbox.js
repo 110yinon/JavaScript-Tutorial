7ec69c5958f0e3a312a143d07ca54cd4b13644ab
unknown <44569804+110yinon@users.noreply.github.com>
2020-08-23 20:25:05 +0300

    my sandbox to web dev callback example

--- sandbox.js
+++ sandbox.js
@@ -13,47 +13,15 @@ function watchTutorialCallback(callback, errorCallback) {
             message: 'WebDevSimplifeid < Cat'
         })
     } else {
-        callback('Thumbs up and subscribe')
+        setTimeout(() => callback('Thumbs up and subscribe'), 2000);
     }
 }
 
-
+console.log(1);
 watchTutorialCallback(msg => {
     console.log('Success: ' + msg);
+    console.log(2);
 }, err => {
     console.log(err.name + ' ' + err.message);
 })
-
-// *****************************************
-// *****************************************
-// *****************************************
-// *****************************************
-// *****************************************
-// *****************************************
-
-function watchTutorialCallbackPromise() {
-    return new Promise((callback, errorCallback) => {
-        if (userLeft) {
-            errorCallback({
-                name: 'User Left',
-                message: ':('
-            })
-        } else if (userWatchingCatMeme) {
-            errorCallback({
-                name: 'User Watching Cat Meme',
-                message: 'WebDevSimplifeid < Cat'
-            })
-        } else {
-            callback('Thumbs up and subscribe')
-        }
-    });
-}
-
-console.log('*****************************');
-
-
-watchTutorialCallbackPromise().then(msg => {
-    console.log('Success: ' + msg);
-}).catch(err => {
-    console.log(err.name + ' ' + err.message);
-})
+console.log(3);
