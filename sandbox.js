c2532006b276fd047184bf8880d9dbedfdeed4d0
unknown <44569804+110yinon@users.noreply.github.com>
2020-08-23 20:26:56 +0300

    my sandbox to web dev promise example

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,29 @@
 const userLeft = false;
 const userWatchingCatMeme = false;
 
-function watchTutorialCallback(callback, errorCallback) {
-    if (userLeft) {
-        errorCallback({
-            name: 'User Left',
-            message: ':('
-        })
-    } else if (userWatchingCatMeme) {
-        errorCallback({
-            name: 'User Watching Cat Meme',
-            message: 'WebDevSimplifeid < Cat'
-        })
-    } else {
-        setTimeout(() => callback('Thumbs up and subscribe'), 2000);
-    }
+function watchTutorialCallbackPromise() {
+    return new Promise((callback, errorCallback) => {
+        if (userLeft) {
+            errorCallback({
+                name: 'User Left',
+                message: ':('
+            })
+        } else if (userWatchingCatMeme) {
+            errorCallback({
+                name: 'User Watching Cat Meme',
+                message: 'WebDevSimplifeid < Cat'
+            })
+        } else {
+            setTimeout(() => callback('Thumbs up and subscribe'), 2000);
+        }
+    });
 }
 
 console.log(1);
-watchTutorialCallback(msg => {
+watchTutorialCallbackPromise().then(msg => {
     console.log('Success: ' + msg);
     console.log(2);
-}, err => {
+}).catch(err => {
     console.log(err.name + ' ' + err.message);
 })
 console.log(3);
