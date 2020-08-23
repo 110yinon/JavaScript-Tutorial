ccb02f1f7011707cc7503c9e706a6691540280b3
unknown <44569804+110yinon@users.noreply.github.com>
2020-08-23 16:53:21 +0300

    use external callback

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,9 @@
 
-const myfunc = (callbackFunc) => {
-    // do something inside myfunc
-    let number = 50;
-    callbackFunc(number); // pass-in number to the callback function
-}
+const peoples = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
-// invoke myfunc
-myfunc(
-    value => {// pass-in callback function
-        // do something inside myfunc
-        console.log('the number is:', value);
-    }
-);
+peoples.forEach(people => console.log(`hello ${people}`));
+
+// Same
+const logPeople = people => console.log(`hello ${people}`);
+
+peoples.forEach(logPeople);
