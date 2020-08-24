6117ab706616419ba03b98921a107498375a3d0a
unknown <44569804+110yinon@users.noreply.github.com>
2020-08-24 13:02:07 +0300

    the this keyword and shorthand to regular function

--- sandbox.js
+++ sandbox.js
@@ -7,11 +7,25 @@ let user = {
     location: 'berlin',
     blogs: ['why mac & cheese rules', '10 things to do with marmite'],
     login: () => console.log('the user logged in'),
-    logout: () => console.log('the user logged out')    
+    logout: () => console.log('the user logged out'),
+    logBlogs: function () {
+        console.log(this);// refers to user
+    },
+    logBlogs2: () => {
+        console.log(this);// refers to window !!!
+    },
+
+    // Shorthand version for Regular function
+    // An easier way to write method in object:
+    logBlogs3(){
+        console.log(this);// refers to user
+    }
 };
 
-user.login();
-user.logout();
+user.logBlogs();
+user.logBlogs2();
+user.logBlogs3();
+console.log(this);// refers to window, the global object in JavaScript
 
 
 
