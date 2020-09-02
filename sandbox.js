7ed0be1053467fa066fbaedf424493e9c65bb757
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-02 17:44:07 +0300

    creating and removing elements

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,18 @@
-const article = document.querySelector('article');
+const ul = document.querySelector('ul');
 const button = document.querySelector('button');
 
-button.addEventListener('click', () => console.log('you clicked me '));
+button.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.textContent = 'something new to do';
+    ul.prepend(li); // put the element on the top of the parent elements list
+    // ul.append(li); // put the element on the bottom of the parent elements list
+});
 
 const items = document.querySelectorAll('li');
 
 items.forEach(item => {
     item.addEventListener('click', e => {
-        // console.log(e);
-        console.log(item);
-        console.log(e.target); // same as item
-
-        item.style.textDecoration = 'line-through';
+        // item.remove();
+        e.target.remove(); // same
     });
 });
