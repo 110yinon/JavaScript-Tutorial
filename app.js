4e7d009feddc51087c649e02db695f5c3aea6048
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-06 18:39:07 +0300

    the window object

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', e => {
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value,];
 
     userAnswers.forEach((answer, index) => {
-        if(answer === correctAnswers[index]){
+        if (answer === correctAnswers[index]) {
             score += 25;
         }
     });
@@ -17,3 +17,18 @@ form.addEventListener('submit', e => {
     result.querySelector('span').textContent = `${score}%`;
     result.classList.remove('d-none');
 });
+
+
+// window object (global object)
+
+console.log('hello');
+window.console.log('hello'); // same
+
+console.log(document.querySelector('form'));
+console.log(window.document.querySelector('form'));
+
+alert('hello');
+window.alert('hello');// same
+
+setTimeout(() => { alert('hey'); }, 3000);
+window.setTimeout(() => { alert('hey'); }, 3000);
