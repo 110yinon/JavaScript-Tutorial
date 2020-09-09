dc3b16e23bab803024c1c1aae64894761259faee
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-09 14:38:28 +0300

    sort method

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,40 @@
-// find the first score above 50
-const scores = [10, 5, 0, 40, 60, 10, 20, 70];
+// example 1 - sorting strings
+const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
 
-const x = scores.find(score => score > 50);
-console.log(x);// 60
+names.sort(); // built-in sort strings alphabetically
+console.log(names);
+
+// example 2 - sorting numbers
+const scores = [10, 50, 20, 5, 35, 70, 45];
+
+scores.sort(); // built-in sort nubmers
+console.log(scores); // [10, 20, 35, 45, 5, 50, 70]
+
+scores.sort((a, b) => a - b);
+console.log(scores); // [5, 10, 20, 35, 45, 50, 70]
+
+// example 3 - sorting objects
+const players = [
+    { name: 'mario', score: 20 },
+    { name: 'luigi', score: 10 },
+    { name: 'chun-li', score: 50 },
+    { name: 'yoshi', score: 30 },
+    { name: 'shaun', score: 70 },
+];
+// opt 1
+players.sort((a, b) => {
+    if (a.score > b.score) {
+        return -1;
+    } else if (a.score < b.score) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+});
+console.log(players);
+
+// opt 2
+players.sort((a, b) => b.score - a.score);
+
+console.log(players);
