affaf792f62c7faa8b707dfd88a9c76e323c4ce0
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-09 11:57:06 +0300

    reduce method

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,28 @@
-const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+const prices = [10, 20, 60, 40, 70, 90, 30];
 
-const salePrices = prices.map(price => price / 2);
+const count = prices.reduce((acc, curr) => {
+    if (curr > 50) {
+        acc++;
+    }
+    return acc;
+}, 0);
 
-console.log(prices);
-console.log(salePrices);
+console.log(count); // 3
+
+// add up the total score by mario
+const scores = [
+    { player: 'mario', score: 50 },
+    { player: 'yoshi', score: 30 },
+    { player: 'mario', score: 70 },
+    { player: 'crystal', score: 60 }
+];
+
+const marioTotal = scores.reduce((acc, curr) => {
+    if (curr.player === 'mario') {
+        acc += curr.score;
+    }
+    return acc;
+}, 0);
+
+console.log(marioTotal);
+
