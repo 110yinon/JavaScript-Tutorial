52b3aef1733e6004f9497061ee81aca8a0181d70
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-14 20:50:10 +0300

    the fetch API

--- sandbox.js
+++ sandbox.js
@@ -1,34 +1,13 @@
-const getTodos = resource => {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-
-        request.addEventListener('readystatechange', () => {
-            // console.log(request,request.readyState);
-            if (request.readyState === 4 && request.status === 200) {
-                // console.log(request.responseText);
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-            } else if (request.readyState === 4) {
-                // console.log('could not fetch the data');
-                reject('could not fetch the data');
-            }
-        });
-
-        request.open('GET', resource);
-        request.send();
-    });
-};
-
-getTodos('todos/luigi.json')
-    .then(data => {
-        console.log(data);
-        return getTodos('todos/mario.json');
+// the fetch API
+fetch('todos/luigi.json')// wrong resource
+    .then(response => {
+        if (response.status === 404) {
+            console.log('404 error, source not found');
+        }
+        else {
+            console.log('resolve', response);
+        }
+        return response.json();
     })
-    .then(data => {
-        console.log(data);
-        return getTodos('todos/shaun.json');
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => console.log(err))
+    .then(data => console.log(data))
+    .catch(err => { console.log('reject', err) })
