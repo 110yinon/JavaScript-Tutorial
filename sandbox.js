8ef1476d72bfb7996fb3142e4878de250409d634
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-14 15:26:50 +0300

    chaining promises

--- sandbox.js
+++ sandbox.js
@@ -1,44 +1,34 @@
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = resource => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange', () => {
-        // console.log(request,request.readyState);
-        if (request.readyState === 4 && request.status === 200) {
-            // console.log(request.responseText);
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            // console.log('could not fetch the data');
-            callback('could not fetch the data', undefined);
-        }
+        request.addEventListener('readystatechange', () => {
+            // console.log(request,request.readyState);
+            if (request.readyState === 4 && request.status === 200) {
+                // console.log(request.responseText);
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                // console.log('could not fetch the data');
+                reject('could not fetch the data');
+            }
+        });
+
+        request.open('GET', resource);
+        request.send();
     });
-
-    request.open('GET', resource);
-    request.send();
 };
 
-// promise example
-const getSomething = () => {
-    return new Promise((resolve, reject) => {
-        // fetch something
-        resolve('some data');
-        // reject('error');
-    });
-}
-
-// opt 1
-// getSomething()
-//     .then(data => {
-//         console.log(data);
-//     }, err => {
-//         console.log(err);
-//     })
-
-// opt 2 - BETTER
-getSomething()
+getTodos('todos/luigi.json')
+    .then(data => {
+        console.log(data);
+        return getTodos('todos/mario.json');
+    })
+    .then(data => {
+        console.log(data);
+        return getTodos('todos/shaun.json');
+    })
     .then(data => {
         console.log(data);
     })
-    .catch(err => {
-        console.log(err);
-    })
+    .catch(err => console.log(err))
