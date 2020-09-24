3466c191e456b8b8ca53ee4e76d5562f900678a7
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-24 20:07:06 +0300

    updating the location

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,22 @@
 const cityForm = document.querySelector("form");
 
+const updateCity = async (city) => {
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    return { cityDetails, weather };
+};
+
 cityForm.addEventListener('submit', e => {
     e.preventDefault();
     // console.log(e.target.location.value);// cityForm.location.value
-    
+
     const city = e.target.location.value;
     cityForm.reset();
 
-    getCity(city)
-    .then(data => {
-        return getWeather(data.Key)
-    })
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+    updateCity(city)
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
 
 });
 
