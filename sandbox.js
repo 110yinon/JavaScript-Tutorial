df2bb6a720fcef26a4bfcca8399f6971f6b575cb
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-30 01:04:46 +0300

    another option to add an event listener

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,16 @@
 const article = document.querySelector('article');
 const button = document.querySelector('button');
 
-button.addEventListener('click', () => console.log('you clicked me '));
+button.onclick = () => console.log('you clicked me ');
 
 const items = document.querySelectorAll('li');
 
 items.forEach(item => {
-    item.addEventListener('click', e => {
+    item.onclick = e => {
         // console.log(e);
         console.log(item);
         console.log(e.target); // same as item
 
         item.style.textDecoration = 'line-through';
-    });
+    };
 });
