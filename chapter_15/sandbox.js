807522ea18315cb85bb2eb3fdbc36300c9e3634b
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-30 20:46:17 +0300

    class methods and methods chaining

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -3,13 +3,32 @@ class User {
         // set up properties
         this.username = username;
         this.email = email;
+        this.score = 0;
+    }
+    login(){
+        console.log(`${this.username} just logged in`);
+        return this;// default function return is undefined
+    }
+    logout(){
+        console.log(`${this.username} just logged out`);
+        return this;
+    }
+    incScore(){
+        this.score++;
+        console.log(`${this.username} has a score of ${this.score}`);
+        return this;
     }
 }
 // creating an instances of User class
 const userOne = new User('mario', 'mario@thenetninja.co.uk');
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
 
-console.log(userOne);
+// console.log(userOne);
+// console.log(userTwo);
+// userOne.login();
+// userTwo.login();
+
+userOne.login().incScore().incScore().logout();
 
 // the 'new' keyword
 // 1 - it creates a new empty object {}
