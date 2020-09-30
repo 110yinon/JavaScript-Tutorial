8bb39e4dc16ecb04d08f1ccd51866c0301d8a1e1
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-30 21:08:33 +0300

    class inheritance (subclasses)

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -5,32 +5,41 @@ class User {
         this.email = email;
         this.score = 0;
     }
-    login(){
+    login() {
         console.log(`${this.username} just logged in`);
         return this;// default function return is undefined
     }
-    logout(){
+    logout() {
         console.log(`${this.username} just logged out`);
         return this;
     }
-    incScore(){
+    incScore() {
         this.score++;
         console.log(`${this.username} has a score of ${this.score}`);
         return this;
     }
 }
+
+class Admin extends User {
+    // if dont have its own ctor
+    // it automatically calls the ctor of extended class
+    
+    deleteUser(user) {
+        users = users.filter(u => user !== u);
+    }
+}
+
+
 // creating an instances of User class
 const userOne = new User('mario', 'mario@thenetninja.co.uk');
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk');
 
-// console.log(userOne);
-// console.log(userTwo);
-// userOne.login();
-// userTwo.login();
 
-userOne.login().incScore().incScore().logout();
+console.log(userOne, userTwo, userThree);
 
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 - it binds the value of 'this' to the new empty object
-// 3 - it calls the constructor function to 'build' the object
+let users = [userOne, userTwo, userThree];
+console.log(users);
+
+userThree.deleteUser(userTwo);
+console.log(users);
