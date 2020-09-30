b7c4176922e82415050ffa2f2c8ba884231c5bf0
unknown <44569804+110yinon@users.noreply.github.com>
2020-09-30 19:56:02 +0300

    class constructor

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,32 +1,17 @@
-const userOne = {
-    username: 'ryu',
-    email: 'ryu@thenetninja.co.uk',
-    login() {
-        console.log('the user logged in');
-    },
-    logout() {
-        console.log('the user logged out');
+class User {
+    constructor(username, email) {
+        // set up properties
+        this.username = username;
+        this.email = email;
     }
-};
+}
+// creating an instances of User class
+const userOne = new User('mario', 'mario@thenetninja.co.uk');
+const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
 
-console.log(userOne.email, userOne.username);
-userOne.login();
+console.log(userOne);
 
-const userTwo = {
-    username: 'chun li',
-    email: 'chun.li@thenetninja.co.uk',
-    login() {
-        console.log('the user logged in');
-    },
-    logout() {
-        console.log('the user logged out');
-    }
-};
-
-console.log(userTwo.email, userTwo.username);
-userTwo.login();
-
-// the code above is NOT very maintainable
-
-// we want somthing like this:
-// const userThree  = new userOne('shaun@thenetninja.co.uk', 'shaun');
+// the 'new' keyword
+// 1 - it creates a new empty object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructor function to 'build' the object
