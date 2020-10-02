5a33c485ac685f3d54a2607940d635a8e10d4f1c
unknown <44569804+110yinon@users.noreply.github.com>
2020-10-02 17:17:39 +0300

    Prototypal Inheritance

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -13,8 +13,24 @@ User.prototype.logout = function () {
     console.log(`${this.username} has logged out`);
 };
 
+function Admin(username, email, title) {
+    User.call(this, username, email);
+    this.title = title;
+}
+
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function () {
+    // delete a user
+    console.log('delete a user');
+};
+
+
 const userOne = new User('mario', 'mario@thenetninja.co.uk');
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk', 'black-belt-ninja');
 
-console.log(userOne, userTwo);
-userOne.login();
+console.log(userThree);
+
+userThree.login();
