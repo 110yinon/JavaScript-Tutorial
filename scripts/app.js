85484beee78f30bbd24d146878736c0ca7ab44bf
unknown <44569804+110yinon@users.noreply.github.com>
2020-10-13 12:46:58 +0300

    144. Sending New Chats

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 // DOM queries
 const chatList = document.querySelector('.chat ul');
+const newChatForm = document.querySelector('.msg-input');
+
+
+// addd a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err))
+
+});
 
 // class instances
 const chatroom = new Chat('gaming', 'shaun');
